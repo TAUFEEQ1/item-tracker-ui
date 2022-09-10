9ace5848a22fcea6c78a63297792377341469af1
Nsamba Taufeeq <nsambataufeeq@gmail.com>
2022-09-11 00:40:21 +0300

    init project

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {Card,CardBody,Jumbotron} from "sveltestrap";
+</script>
+
+<main class="d-flex align-items-center justify-content-center">
+  <Card>
+    <CardBody>
+      
+    </CardBody>
+  </Card>
+</main>
+
+<style>
+</style>
